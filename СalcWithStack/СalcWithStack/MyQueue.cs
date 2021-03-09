f572c08c05cf98f34f6d2721aa50935417cdc1cd
ArtyomKiselyov <78850376+ArtyomKiselyov@users.noreply.github.com>
2021-03-09 22:33:27 +0300

    Add files via upload
    Калькулятор с использование обратной польской записи

--- СalcWithStack/СalcWithStack/MyQueue.cs
+++ СalcWithStack/СalcWithStack/MyQueue.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace СalcWithStack
 {
-    public class MyStack<T>
+    class MyQueue<T>
     {
         private T[] items;
         private int count;
         const int n = 10;
-        public MyStack()
+        public MyQueue()
         {
             items = new T[n];
         }
@@ -23,31 +23,50 @@ namespace СalcWithStack
         {
             get { return count; }
         }
-        public void Push(T item)
+
+        public void Enqueue(T item)
         {
             if (count == items.Length)
                 Array.Resize(ref items, items.Length + 1);
             items[count++] = item;
+            ////if (count > n)
+            //Array.Resize(ref items, count + 10);
+            ////count++;
+            //items[items.Length] = item;
+            //count++;
         }
-        public T Pop()
+        public T Dequeue()
         {
             if (!IsEmpty)
             {
                 if (IsEmpty)
-                    throw new InvalidOperationException("Стек пуст");
-                T item = items[--count];
+                    throw new InvalidOperationException("Очередь пуста");
+                T item = items[0];
+                for (int i = 0; i < count - 1; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                count--;
+
                 items[count] = default(T);
                 return item;
             }
             else return default;
+            
         }
         public T Peek()
         {
-            if (!IsEmpty) 
+            if (!IsEmpty)
             {
-                return items[count - 1]; 
+                return items[0];
             }
             else return default;
+            
+        }
+        public void Clear()
+        {
+            Array.Resize(ref items, count - count);
+            count = 0;
         }
     }
 }
